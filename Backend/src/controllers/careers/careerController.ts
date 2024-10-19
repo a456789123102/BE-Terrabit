e178242f34ffd32aec3346b4d6940165d2380a24
456789123102 <aize.gong@gmail.com>
2024-10-19 22:13:02 +0700

    finished career path

--- Backend/src/controllers/careers/careerController.ts
+++ Backend/src/controllers/careers/careerController.ts
@@ -25,3 +25,44 @@ export const createCareer = async (req: Request, res: Response) => {
         return res.status(500).json({ message: "Error creating career", error });
     }
 }
+
+export const getAllCareers = async (req: Request, res: Response) => {
+    try {
+        console.log("Career_getAll");
+        const careers = await prisma.career.findMany({
+            select: {
+                id: true,  
+                title: true,  
+                site: true, 
+                type: true,  
+                location: true,
+                salary: true,
+                createdAt: true, 
+              },
+            orderBy: { createdAt: "desc" },
+            
+        });
+        res.status(200).json(careers);
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching careers", error });
+        throw error;
+    }
+}
+
+export const getCareerById = async (req:Request, res:Response) => {
+    try {
+        ("Career_getOneById")
+        const { id } = req.params;
+        const careerId = parseInt(id);
+        const career = await prisma.career.findUnique({
+            where: { id: careerId },
+        });
+        res.status(200).json(career);
+        if (!career) {
+            return res.status(404).json({ message: "Career not found" });
+        }
+    } catch (error) {
+        console.error('error fetching career',error);
+        throw error;
+    }
+}
--- Backend/src/routes/career/careerRoute.ts
+++ Backend/src/routes/career/careerRoute.ts
@@ -1,7 +1,9 @@
-import { createCareer } from "../../controllers/careers/careerController";
+import { createCareer,getCareerById, getAllCareers } from "../../controllers/careers/careerController";
 import { verifyUser, verifyAdmin } from "../../middlewares/verify";
 import {Router} from "express";
 
 const router = Router();
 router.post('/create',verifyAdmin,createCareer);
+router.get('/:id',getCareerById);
+router.get('/',getAllCareers);
 export default router;
