f94fb110a156713070f51e3d6d5be0032726e4aa
456789123102 <aize.gong@gmail.com>
2024-11-20 22:08:32 +0700

    get personal cart cannot find userId need to fix

--- Backend/src/controllers/carts/cartController.ts
+++ Backend/src/controllers/carts/cartController.ts
@@ -2,11 +2,15 @@ import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
+//create
 export const createCart = async (req: Request, res: Response) => {
   console.log("Cart_create");
   try {
-    const { userId, productId, quantity, totalPrice } = req.body;
+    const userId = parseInt(req.params.userId, 10);
+    if (isNaN(userId)) {
+        return res.status(400).json({ message: "Invalid userId. It must be a number." });
+      }
+    const { productId, quantity, totalPrice } = req.body;
     const cartItem = await prisma.cart.findFirst({
       where: { userId, productId },
     });
@@ -23,8 +27,37 @@ export const createCart = async (req: Request, res: Response) => {
     const newCart = await prisma.cart.create({
       data: { userId, productId, quantity, totalPrice },
     });
-    return res.status(200).json({ message: "Added new item to cart", newCart });
+    return res.status(201).json({ message: "Added new item to cart", newCart });
   } catch (error) {
     return res.status(500).json({ error: "Failed to add to cart", details: error });
   }
 };
+
+//getAllwebCart for statregic
+export const getAllCart =  async (req:Request, res:Response) => {
+    console.log("Cart_getAll")
+    try {
+        const cartItems = await prisma.cart.findMany({});
+        return res.status(200).json(cartItems)
+    } catch (error) {
+        return res.status(500).json({ error: "Failed to get cart", details: error })
+    }
+}
+
+//getPersonalcart
+export const getPersonalCart = async (req:Request, res:Response) =>{
+    console.log("Cart_getPersonal")
+    try {
+        const userId = parseInt(req.params.userId, 10);
+        if (isNaN(userId)) {
+            console.log("req.params:", req.params);
+            return res.status(400).json({ message: "Invalid userId. It must be a number.",userId });
+        }
+        const cartItems = await prisma.cart.findMany({
+            where: { userId},
+        });
+        return res.status(200).json(cartItems);
+    } catch (error) {
+        return res.status(500).json({ error: "Failed to get personal cart", details: error })
+    }
+}
--- Backend/src/routes/cart/cartRoute.ts
+++ Backend/src/routes/cart/cartRoute.ts
@@ -1,7 +1,10 @@
-import { createCart } from "../../controllers/carts/cartController";
+import { createCart,getAllCart,getPersonalCart } from "../../controllers/carts/cartController";
 import { verifyUser, verifyAdmin } from "../../middlewares/verify";
 import {Router} from 'express';
 
 const router = Router();
 router.post('/add',verifyUser,createCart);
+router.get('/getAll',verifyAdmin,getAllCart);
+router.get('/myCart',verifyUser,getPersonalCart);
+
 export default router;
