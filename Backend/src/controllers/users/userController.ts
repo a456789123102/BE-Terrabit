5cc7cc6c33a310822f51841cbf8ce821f50c04bc
456789123102 <aize.gong@gmail.com>
2025-02-18 15:37:44 +0700

    add changeuserStatus

--- Backend/src/controllers/users/userController.ts
+++ Backend/src/controllers/users/userController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -12,12 +12,15 @@ export const getAllUsers = async (req: Request, res: Response) => {
     const search = req.query.search as string | undefined;
     const orderBy = (req.query.orderBy as "asc" | "desc") || "desc";
     const orderWith = (req.query.orderWith as string) || "createdAt";
-    
-    const page = Math.max(Number(req.query.page) || 1, 1); 
-    const isActiveParam = req.query.isActive as string ;
+
+    const page = Math.max(Number(req.query.page) || 1, 1);
+    const isActiveParam = req.query.isActive as string;
     const isActive =
-    isActiveParam !== undefined ? isActiveParam === "true" : undefined;
-    const pageSize = Math.min(Math.max(Number(req.query.pageSize) || 10, 1), 100); // Default to 10, max 100
+      isActiveParam !== undefined ? isActiveParam === "true" : undefined;
+    const pageSize = Math.min(
+      Math.max(Number(req.query.pageSize) || 10, 1),
+      100
+    ); // Default to 10, max 100
     const offset = (page - 1) * pageSize;
 
     // Prisma Query
@@ -26,12 +29,12 @@ export const getAllUsers = async (req: Request, res: Response) => {
       take: pageSize,
       where: {
         username: search ? { contains: search.toLowerCase() } : undefined,
-        isActive: isActive !== undefined ? isActive : undefined,  // ✅ แก้ไขตรงนี้
+        isActive: isActive !== undefined ? isActive : undefined, // ✅ แก้ไขตรงนี้
       },
       orderBy: {
         [orderWith]: orderBy,
       },
-      select:{
+      select: {
         id: true,
         email: true,
         username: true,
@@ -40,11 +43,10 @@ export const getAllUsers = async (req: Request, res: Response) => {
         updatedAt: true,
       },
     });
-    
-    
+
     const totalUsers = await prisma.user.count({
       where: {
-        username: search ? { contains: search.toLowerCase()} : undefined,
+        username: search ? { contains: search.toLowerCase() } : undefined,
       },
     });
 
@@ -66,20 +68,51 @@ export const getAllUsers = async (req: Request, res: Response) => {
   }
 };
 
+//Find Own Users
+export const me = async (req: Request, res: Response) => {
+  try {
+    console.log("user_me");
+    const userId = (req as any).user.id;
+    const users = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        isAdmin: true,
+        createdAt: true,
+      },
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error while getting users", error });
+  }
+};
+//////////////////////////////////
 
-  //Find Own Users
-  export const me = async (req: Request, res: Response) => {
-    try {
-      console.log("user_me")
-      const userId = (req as any).user.id;
-      const users = await prisma.user.findUnique({
-        where: { id: userId },
-        select: { id: true, email: true, username: true, isAdmin: true, createdAt: true }
-      });
-      res.status(200).json(users);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error while getting users',error });
+export const changeIsActiveStatus = async (req: Request, res: Response) => {
+  try {
+    console.log("user_changeIsActiveStatus");
+    const id = parseInt(req.params.id);
+    const currentStatus = await prisma.user.findFirst({
+      where: { id: id },
+    });
+    if (!currentStatus) {
+      return res.status(404).json({ message: "User not found" });
     }
-  };
-  
+    await prisma.user.update({
+      where: { id: id },
+      data: {
+        isActive: !currentStatus.isActive,
+      },
+    });
+
+    res.status(200).json({ message: `User with ID ${id} has been updated.` });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Error while updating user status", error });
+  }
+};
--- Backend/src/routes/users/userRoute.ts
+++ Backend/src/routes/users/userRoute.ts
@@ -1,4 +1,4 @@
-import {me,getAllUsers } from "../../controllers/users/userController"; 
+import {me,getAllUsers,changeIsActiveStatus } from "../../controllers/users/userController"; 
 import { Router } from "express";
 import { verifyUser,verifyAdmin } from "../../middlewares/verify";
 import {getWeeklyUserForCharts} from "../../controllers/users/userAnalyticsService";
@@ -8,4 +8,6 @@ const router = Router();
 router.get('/me',verifyUser, me);
 router.get('/usersInfo',verifyAdmin,getAllUsers);
 router.get('/charts/getWeeklyUserForCharts',verifyAdmin,getWeeklyUserForCharts);
+router.patch("/:id/changeIsActiveStatus",verifyAdmin,changeIsActiveStatus);
+
 export default router;
